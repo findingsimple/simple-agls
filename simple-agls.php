dddb689476fc790d15804b8d44ec8c99e1a7af66
Jason Conroy <jason@findingsimple.com>
2012-06-11 17:23:23 +1000

    Subject scheme -> AGLSTERMS.TAGS

--- simple-agls.php
+++ simple-agls.php
@@ -782,7 +782,7 @@ class SIMPLE_AGLS {
 			);
 			
 		if (get_option('simple_agls-toggle-scheme-attribute') == 1) {
-			$attributes['scheme'] = ' ';
+			$attributes['scheme'] = 'AGLSTERMS.TAGS';
 		}
 			
 		if ( !$echo && !empty($attributes) )
